c98bdcf878e0482ea1475b29497285d967995575
Alok Rai <dkrai1276@gmail.com>
2024-11-15 23:00:02 +0530

    frontend started

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <H2>Order your favourite food here</H2>
+        <p>Choose from as diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time.</p>
+        <button>View <Menu></Menu></button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
